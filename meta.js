77dff69bd64f11af5c017ae379171fa1ef1792b8
nickolas <kolanlos510@gmail.com>
2020-02-03 23:15:30 +0300

    add libs

---
+++ meta.js
@@ -0,0 +1,9 @@
+module.exports = {
+    prompts: {
+      name: {
+        type: 'string', // Question type
+        required: true, // Is question Required
+        message: 'vue-smart-box' // Question it's self
+      },
+    },
+  };
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>This template has</h1>
+        <ul>
+            <li v-for="(name, index) in packages" :key="index">{{name}}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    name: "app",
+    data() {
+        return {
+            packages: [
+                "vue-devtools",
+                "vue-router",
+                "vuex",
+                "sass",
+                "vuelidate"
+            ]
+        };
+    }
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.h1 {
+    color: red;
 }
 </style>
