6e43210350cf1f494fe0ad32cb16d91ee452fccf
nickolas <kolanlos510@gmail.com>
2020-02-04 02:25:49 +0300

    fix

--- template/src/views/Home.vue
+++ template/src/views/Home.vue
@@ -2,7 +2,7 @@
     <div>
         <h2>this template has</h2>
         <ul>
-            <li v-for="(pack, index) in packages" :key="index">
+            <li v-for="(pack, index) in packages" :key="index" v-text="pack">
                 {{pack}}
             </li>
         </ul>
